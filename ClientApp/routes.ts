75d5dd0514bec0884cb9ae9f68083f8b352941e3
edle <eduard.lepner@powel.no>
2016-08-18 17:21:46 +0200

    Added clients to naviagion and routes.

--- ClientApp/routes.ts
+++ ClientApp/routes.ts
@@ -2,11 +2,12 @@ import { RouterConfig } from '@angular/router';
 import { Home } from './components/home/home';
 import { FetchData } from './components/fetch-data/fetch-data';
 import { Counter } from './components/counter/counter';
-
+import {Clients} from './components/clients/clients';
 export const routes: RouterConfig = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: Home },
     { path: 'counter', component: Counter },
     { path: 'fetch-data', component: FetchData },
+    { path: 'clients', component: Clients },
     { path: '**', redirectTo: 'home' }
 ];
